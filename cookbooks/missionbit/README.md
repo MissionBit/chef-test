559e429b90699a99066f60f19cf5fec82640289e
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2018-09-08 21:24:13 -0700

    Initial commit

---
+++ cookbooks/missionbit/README.md
@@ -0,0 +1,4 @@
+# missionbit
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/missionbit/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: missionbit
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+bash 'test'  do
+   code "ls -la"
+end
+
+
