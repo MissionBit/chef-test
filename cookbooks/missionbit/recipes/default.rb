5c70cc7fa8b3042bf29a7deaff0f8ad9f624d748
Colin Bean <colin@missionbit.com>
2018-09-10 21:44:44 -0700

    Fix syntax error

--- cookbooks/missionbit/recipes/default.rb
+++ cookbooks/missionbit/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 bash 'run munki'  do
-   code "defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL #{	node['cpe_munki']['preferences']['SoftwareRepoURL']} &&  && /usr/local/munki/managedsoftwareupdate && /usr/local/munki/managedsoftwareupdate --installonly"
+   code "defaults write /Library/Preferences/ManagedInstalls SoftwareRepoURL #{	node['cpe_munki']['preferences']['SoftwareRepoURL']} && /usr/local/munki/managedsoftwareupdate && /usr/local/munki/managedsoftwareupdate --installonly"
 end
 
 
